9289cdc770c0d7cf016e6d51d66cfaf0abb7a589
srush <srush.research@gmail.com>
2021-06-02 12:47:34 -0400

    update notebooks

---
+++ notebooks/module0.py
@@ -0,0 +1,65 @@
+# Lab 0
+
+# As part of the summer session, we will build a machine
+# learning system for an applied problem. Each person
+# will work through their lab in a notebook with their group
+# members. 
+
+
+# ## Lab Schedule
+
+# Each week for the next 8 weeks we will do a lab.  Labs are a
+# combination of ML background and applications.
+
+
+# Planned Labs
+#
+# 1) Programming (Python)
+# 2) Structured Data (Pandas)
+# 3) Visualizing Data (Altair)
+# 4) Machine Learning I (Scikit-Learn)
+# 5) Machine Learning II (Scikit-Learn)
+# 6) Deep Learning I (Keras)
+# 7) Deep Learning II (Keras)
+# 8) AI Applications (Keras)
+
+
+
+# ## Lab Structure
+
+# Each lab will last for 3 hours. It will be broken up into
+# two parts with a 10 minute break in between. Each lab will
+# end with a short feedback period at the end. 
+
+# During the lab there will be 2 group sessions where we will
+# break into small groups. Groups will be randomly assigned each
+# week.
+
+
+# ## Sample Lab
+
+# 4:05-4:15 - Icebreakers and Weekly Teams
+# 4:15-4:25 - Review
+# 4:25-4:30 - Self-Review Exercise
+# 4:30-5:00 - Unit A Walkthrough
+# 5:00-5:35 - Group Exercise A
+# 5:35-5:45 - Break
+# 5:45-6:15 - Unit B Walkthrough
+# 6:15-6:45 - Group Exercise B
+# 6:45-6:55 - Wrap-up
+# 6:55-7:00 - Class Survey
+
+
+# ## Colab Notebooks
+
+# For the course we will use a tool known as Colab notebooks.
+# This is a special system developed by Google that is
+# designed to be a professional AI development environment.
+
+
+# To get started, you will need to follow these steps:
+
+# 1) Be sure to have a Google account. If you use gmail
+#    that should work to get started.
+# 2) Navigate to  
+
